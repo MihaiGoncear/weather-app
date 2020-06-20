e745e04e1ef111c81f9f06acbf3012ea84af1567
MihaiGoncear <MihaiGoncear@users.noreply.github.com>
2020-06-20 17:41:09 +0300

    --

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+import "../styles/style.scss"
+
+// let currentDate = document.getElementById('date');
+
+// function dateFormatter () {
+//     var now = this.date;
+//     var y = now.getFullYear();
+//     var m = now.getMonth() + 1;
+//     var d = now.getDate();
+//     var mm = m < 10 ? '0' + m : m;
+//     var dd = d < 10 ? '0' + d : d;
+//     return '' + y + '-' + mm + '-' + dd;
+// }
+// console.log(dateFormatter())
+// currentDate.innerText = new Date();
+console.log('test')
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-
+const {CleanWebpackPlugin} = require('clean-webpack-plugin');
 module.exports = {
     entry:'./src/js/index.js',
     output: {
@@ -14,6 +14,9 @@ module.exports = {
                 test: /\.(sa|sc|c)ss$/,
                 use: [
                     {
+                        loader: 'css-hot-loader',
+                    },
+                    {
                         loader: MiniCssExtractPlugin.loader,
                     },
                     'css-loader',
@@ -22,7 +25,20 @@ module.exports = {
             },
         ]
     },
-    plugins: [new MiniCssExtractPlugin({
-        filename: 'styles.css'
-    })],
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: 'styles.css'
+        }), 
+        new CopyWebpackPlugin({
+            patterns: [
+                {from: 'index.html', to: 'index.html'}
+            ]
+        }),
+    ],
+    devServer: {
+        contentBase: path.resolve(__dirname, 'build'),
+        compress: true,
+        port: 9000,
+    }
 };
