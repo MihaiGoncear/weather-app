2b8daf3e6f9bdea96290d146afd130d6973a89fb
MihaiGoncear <MihaiGoncear@users.noreply.github.com>
2020-06-20 15:58:38 +0300

    --

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
-    entry:'./src/js/bundle.js',
+    entry:'./src/js/index.js',
     output: {
         path: path.resolve(__dirname, 'build'),
         filename: 'bundle.js'
