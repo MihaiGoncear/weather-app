1e382800d439ca4f78e9327bdf42608668395881
MihaiGoncear <MihaiGoncear@users.noreply.github.com>
2020-06-23 16:20:00 +0300

    --

--- src/js/api.js
+++ src/js/api.js
@@ -1,8 +1,11 @@
 export function fetchWeatherApi(){
     fetch('http://api.openweathermap.org/data/2.5/group?id=6618607,618426,524901,7535661,5039192&units=metric&appid=b8b5407d289db4e2cbfa0f2bbc1a70ce')
         .then(response => response.json())
-        .then(data => console.log(data))
+        .then(data => createContent())
 }
 
+function createContent(){
+    
+}
 
 
